4513856873036e4146f0357f2041ad94dcba11ca
mark <markpackham1@gmail.com>
2020-09-04 16:24:07 +0100

    learning to modularize the code, https://youtu.be/5L5R4kpfYxw?t=79

---
+++ webpack.config.js
@@ -0,0 +1,19 @@
+const path = require("path");
+
+module.exports = {
+  entry: "./src/index.ts",
+  module: {
+    rules: [
+      {
+        test: /\.ts$/,
+        include: [path.resolve(__dirname, "src")],
+        use: "ts-loader",
+      },
+    ],
+  },
+  output: {
+    publicPath: "public",
+    filename: "bundle.js",
+    path: path.resolve(__dirname, "public"),
+  },
+};
