525c29591589657b39fd840907d7a6c0c4845845
mark <markpackham1@gmail.com>
2020-09-04 16:42:26 +0100

    bundle files into one file

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,9 @@
-console.log("Hello");
-console.log("Hello again");
+import { formData } from "./form";
+
+const form = document.querySelector("form")!;
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  const data = formData(form);
+  console.log(data);
+});
